53732e975ee4cfe6dd2bde8be04c77d5ab549f77
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-05-24 22:29:54 -0500

    Se termino con todas las locaciones que teniamos D, B, P cumpliento el objetivo = 0

--- Agente.py
+++ Agente.py
@@ -11,37 +11,7 @@ def diagnostico():
     ubicacion_laptop = input("Ingrese la ubicacion para el diagnostico: ") #Ubicacion para el diagnostico
     estado = input("Ingrese el estado del: " +ubicacion_laptop+ " ") #Si se encuentra en 0/1
 
-    if (estado == '1'): #Caso de la parte P este en mal estado
-            print("El diagnostico de P se encuentra en buen estado")
-            if(ubicacion2_estado == '1'):
-                print("El diagnostico de B esta en mal estado")
-                print("El diagnostico empezara ahora en B") 
-                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
-                print("El costo actual es: " +str(costo)) #Costo actual
-                objetivo['B'] ='0' #Diagnostico dara buen estado
-                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
-                print("El diagnostico muestra en buen estado en B")
-                print("El costo actual es: "  +str(costo)) #Costo actual
-
-            else: #Caso de la parte B este en buen estado 
-                print("Diagnostico de la parte B se refleja en buen estado")
-                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
-                
-            if(ubicacion3_estado == '1'): #Caso del diagnostico de P este en mal estado
-                print("El diagnostico de P esta en mal estado")
-                print("El diagnostico empezara ahora en P") 
-                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
-                print("El costo actual es: " +str(costo)) #Costo actual
-                objetivo['P'] ='0' #Diagnostico dara buen estado
-                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
-                print("El diagnostico muestra en buen estado en P")
-                print("El costo actual es: "  +str(costo)) #Costo actual
-
-            else:
-                print("Diagnostico de la parte P se refleja en buen estado")
-                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
-
-    elif (ubicacion_laptop  == 'D'):
+    if (ubicacion_laptop  == 'D'):
         ubicacion2_estado = input("Ingrese el estado de B") # Se indica si esta de estado 0/1 en dicha ubicacion.
         ubicacion3_estado = input("Ingrese el estado de P") # Se indica si esta de estado 0/1 en dicha ubicacion.
         print("Objetivo:" + str(objetivo)) #Los objetivos esperados por el agente
@@ -81,9 +51,39 @@ def diagnostico():
             else:
                 print("Diagnostico de la parte P se refleja en buen estado")
                 print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
-        else:
-            #El estado ingresado esta fuera del rango
-            print("El estado ingresado no es valido")
+
+    elif (estado == '1'): #Caso de la parte P este en mal estado
+            print("El diagnostico de P se encuentra en buen estado")
+            if(ubicacion2_estado == '1'):
+                print("El diagnostico de B esta en mal estado")
+                print("El diagnostico empezara ahora en B") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['B'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en B")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else: #Caso de la parte B este en buen estado 
+                print("Diagnostico de la parte B se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+                
+            if(ubicacion3_estado == '1'): #Caso del diagnostico de P este en mal estado
+                print("El diagnostico de P esta en mal estado")
+                print("El diagnostico empezara ahora en P") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['P'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en P")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else:
+                print("Diagnostico de la parte P se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene 
+    
+
+
     elif (ubicacion_laptop  == 'B'):
         ubicacion2_estado = input("Ingrese el estado de D") # Se indica si esta de estado 0/1 en dicha ubicacion.
         ubicacion3_estado = input("Ingrese el estado de P") # Se indica si esta de estado 0/1 en dicha ubicacion.
@@ -124,3 +124,114 @@ def diagnostico():
             else:
                 print("Diagnostico de la parte P se refleja en buen estado")
                 print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+
+        elif (estado == '0'): #Caso de la parte P este en mal estado
+            print("El diagnostico de B se encuentra en buen estado")
+            if(ubicacion2_estado == '1'):
+                print("El diagnostico de D esta en mal estado")
+                print("El diagnostico empezara ahora en D") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['D'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en D")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else: #Caso de la parte B este en buen estado 
+                print("Diagnostico de la parte D se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+                
+            if(ubicacion3_estado == '1'): #Caso del diagnostico de P este en mal estado
+                print("El diagnostico de P esta en mal estado")
+                print("El diagnostico empezara ahora en P") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['P'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en P")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else:
+                print("Diagnostico de la parte P se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+        else:
+            #El estado ingresado esta fuera del rango
+            print("El estado ingresado no es valido")
+    
+    elif (ubicacion_laptop  == 'P'):
+        ubicacion2_estado = input("Ingrese el estado de D") # Se indica si esta de estado 0/1 en dicha ubicacion.
+        ubicacion3_estado = input("Ingrese el estado de B") # Se indica si esta de estado 0/1 en dicha ubicacion.
+        print("Objetivo:" + str(objetivo)) #Los objetivos esperados por el agente
+        print("La ubicacion del diagnostico se encuentra en P")# Ubicacion del diagnostico
+        
+        if(estado == '1'): #El dignostico del disco (P) esta en mal estado
+            print("El diagnostico reporta un mal estado")
+            objetivo['P'] ='0' #Diagnostico pasara a 0
+            costo += 1 #Aumenta el costo
+            print("El diagnostico reporta buen estado en el P")
+            print("El costo actual es: " +str(costo)) 
+                
+            if(ubicacion2_estado == '1'):
+                print("El diagnostico de D esta en mal estado")
+                print("El diagnostico empezara ahora en D") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['D'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en D")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else: #Caso de la parte B este en buen estado 
+                print("Diagnostico de la parte D se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+
+            if(ubicacion3_estado == '1'): #Caso del diagnostico de P este en mal estado
+                print("El diagnostico de B esta en mal estado")
+                print("El diagnostico empezara ahora en B") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['B'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en B")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else:
+                print("Diagnostico de la parte B se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+
+        elif (estado == '0'): #Caso de la parte D este en mal estado
+            print("El diagnostico de P se encuentra en buen estado")
+            if(ubicacion2_estado == '1'):
+                print("El diagnostico de D esta en mal estado")
+                print("El diagnostico empezara ahora en D") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['D'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en D")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else: #Caso de la parte B este en buen estado 
+                print("Diagnostico de la parte D se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+                
+            if(ubicacion3_estado == '1'): #Caso del diagnostico de P este en mal estado
+                print("El diagnostico de P esta en mal estado")
+                print("El diagnostico empezara ahora en P") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['P'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en P")
+                print("El costo actual es: "  +str(costo)) #Costo actual
+
+            else:
+                print("Diagnostico de la parte P se refleja en buen estado")
+                print("Diagnostico en buen estado, no se aumenta costo"  +str(costo)) #Costo se mantiene
+        else:
+            #El estado ingresado esta fuera del rango
+            print("El estado ingresado no es valido")
+    #Todo Encendido
+    print("Estado Objetivo: " + str(objetivo))
+    print("Medición del desempeño " +str(costo))
+diagnostico()
