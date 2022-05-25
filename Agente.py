427d7282af2444d4c8b2356f7f4869604b586d84
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-05-24 19:21:04 -0500

    Se realiza el diagnostico en la primera ibicacion (D)

--- Agente.py
+++ Agente.py
@@ -9,10 +9,27 @@ def diagnostico():
     costo = 0 # Costo se inicia en 0
 
     ubicacion_laptop = input("Ingrese la ubicacion para el diagnostico: ") #Ubicacion para el diagnostico
-    ingreso_estado = input("Ingrese el estado del: " +ubicacion_laptop+ " ") #Si se encuentra en 0/1
+    estado = input("Ingrese el estado del: " +ubicacion_laptop+ " ") #Si se encuentra en 0/1
 
     if (ubicacion_laptop  == 'D'):
         ubicacion2_estado = input("Ingrese el estado de B") # Se indica si esta de estado 0/1 en dicha ubicacion.
         ubicacion3_estado = input("Ingrese el estado de P") # Se indica si esta de estado 0/1 en dicha ubicacion.
         print("Objetivo:" + str(objetivo)) #Los objetivos esperados por el agente
-        print("La ubicacion para nuestro diagnostico se encuentra en B")# Ubicacion del diagnostico
+        print("La ubicacion del diagnostico se encuentra en D")# Ubicacion del diagnostico
+        
+        if(estado == '1'): #El dignostico del disco (D) esta en mal estado
+            print("El diagnostico reporta un mal estado")
+            objetivo['D'] ='0' #Diagnostico pasara a 0
+            costo += 1 #Aumenta el costo
+            print("El diagnostico reporta buen estado en el D")
+            print("El costo actual es: " +str(costo)) 
+                
+            if(ubicacion2_estado == '1'):
+                print("El diagnostico de B")
+                print("El diagnostico empezara ahora en B") 
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El costo actual es: " +str(costo)) #Costo actual
+                objetivo['B'] ='0' #Diagnostico dara buen estado
+                costo += 1 #Cada vez que el diagnostico este en buen estado aumentara
+                print("El diagnostico muestra en buen estado en B")
+                print("El costo actual es: "  +str(costo)) #Muestra el costo actual 
